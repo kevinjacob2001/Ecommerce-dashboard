2498003d040102977c5033aaf256f1f506c77593
Kevin Jacob <kevinjacob2001@gmail.com>
2022-01-16 18:24:04 +0530

    Added: Express-handlebars

--- app.js
+++ app.js
@@ -1,6 +1,31 @@
 const express=require('express');
+const exphbs=require('express-handlebars')
+const bodyParser=require('body-parser')
+const mysql=require('mysql')
+
+require('dotenv').config()
+
 const app=express();
 const port=process.env.PORT||5000
 
+//to parser application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended:false}))
+
+//to parse application/json
+app.use(bodyParser.json())
+
+//static files
+app.use(express.static('public'));
+
+//templating engine
+app.engine('hbs',exphbs.engine({extname:'.hbs'}))
+
+//To select which templating engine to use, here hbs
+app.set('view engine','hbs')
+
+//Route
+app.get('/',(req,res)=>
+res.render('home')
+)
 
 app.listen(port,()=>console.log("Listening"))
