6a9dc53611aed22ac6e5606cb23d65187d977fda
Kevin Jacob <kevinjacob2001@gmail.com>
2022-01-16 20:43:34 +0530

    Added: Connection pool

--- app.js
+++ app.js
@@ -23,9 +23,25 @@ app.engine('hbs',exphbs.engine({extname:'.hbs'}))
 //To select which templating engine to use, here hbs
 app.set('view engine','hbs')
 
+//connection pool
+const pool=mysql.createPool({
+    connectionLimit:100,
+    host:process.env.DB_HOST,
+    user:process.env.DB_USER,
+    password:process.env.DB_PASS,
+    database:process.env.DB_NAME})
+
+//cONNECT to DB_NAME
+pool.getConnection((err,connection) =>{
+    if(err) throw err;
+    else console.log("Connected as ID"+connection.threadId)
+})
+
 //Route
 app.get('/',(req,res)=>
-res.render('home')
+{
+    res.render('home')
+}
 )
 
 app.listen(port,()=>console.log("Listening"))
