23b84d50ef2cf77716ccc4c3328b9ea85ca00148
Kevin Jacob <kevinjacob2001@gmail.com>
2022-02-16 21:14:57 +0530

    Added: Insert product

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,44 +1,48 @@
 const mysql = require('mysql')
 
 //connection pool
-const pool = mysql.createPool({
-    connectionLimit: 100,
+let connection = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME
-})
-
+  });
 
 //View users
 exports.view = (req, res) => {
 
     //cONNECT to DB_NAME
-    pool.getConnection((err, connection) => {
-        if (err) throw err;
-        else console.log("Connected as ID" + connection.threadId)
 
         connection.query('SELECT * FROM user', (err, rows) => {
             //when donw with connection release it
-            connection.release();
+        
             if (!err) {
                 res.render('home',{rows})
             }
             else console.log(err)
 console.log(rows)
         })
-    })
+   
 }
-//node js- js
 
 
-//1:03:38
+exports.addProductsForm=(req,res)=>{
+    res.render('addProducts');
+}
 
-//add production
+// Add new user
+exports.add = (req, res) => {
+    const {id,product_name,product_manufacturer,product_quantity,product_category } = req.body;
 
-//update production
-
-//delete
-
-
-//5 table + renaming + UI
+    // User the connection
+    connection.query('INSERT INTO user SET product_name = ?, product_manufacturer = ?, product_quantity = ?, product_category = ?', [product_name,product_manufacturer,product_quantity,product_category], (err, rows) => {
+      if (!err) {
+        res.redirect('/') 
+        res.render('home', {rows});
+         
+      }
+      else console.log(err);
+        console.log('The data from user table: \n', rows);
+    })
+  }
+  
